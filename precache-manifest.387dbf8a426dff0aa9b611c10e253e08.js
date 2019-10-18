530eb3d6e70f4815d9cfe4f4899d78b52df9c60e
Malu <malumeenu.shaji@gmail.com>
2019-10-18 08:34:53 +0400

    Updates

---
+++ precache-manifest.387dbf8a426dff0aa9b611c10e253e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbab3042844fe8f716f2573c7634f49",
+    "url": "/flight-center-app/index.html"
+  },
+  {
+    "revision": "123aa746e567c76719df",
+    "url": "/flight-center-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1fc71781759d5222dcbd",
+    "url": "/flight-center-app/static/css/main.f9bd19f8.chunk.css"
+  },
+  {
+    "revision": "123aa746e567c76719df",
+    "url": "/flight-center-app/static/js/2.88de6cec.chunk.js"
+  },
+  {
+    "revision": "1fc71781759d5222dcbd",
+    "url": "/flight-center-app/static/js/main.1c49442a.chunk.js"
+  },
+  {
+    "revision": "06c05f4fb2e2c14540b6",
+    "url": "/flight-center-app/static/js/runtime-main.88937b04.js"
+  },
+  {
+    "revision": "712b571c8b0a95e0fa5c316bb9a2da14",
+    "url": "/flight-center-app/static/media/airplane.712b571c.svg"
+  }
+]);
